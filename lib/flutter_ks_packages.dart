6150ea2e2610bd2c3b35488130f4b26660f413b4
Kevin <443634449@qq.com>
2022-05-25 15:31:23 +0800

    update flutter_ks_echarts

--- lib/flutter_ks_packages.dart
+++ lib/flutter_ks_packages.dart
@@ -6,3 +6,4 @@ import 'package:flutter_ks_packages/popup/ks_popup_alert_widget.dart';
 import 'package:flutter_ks_packages/popup/ks_popup_custom_widget.dart';
 import 'package:flutter_ks_packages/popup/ks_popup_utils.dart';
 import 'package:flutter_ks_packages/loading/widget_loading.dart';
+import 'package:flutter_ks_packages/echarts/widget_ks_echarts.dart';
